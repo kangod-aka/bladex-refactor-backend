7f2e57094852d7d02f17733a7f7a740768bff8a9
KangShen <yk625x@163.com>
2023-06-01 15:37:58 +0800

    demo-c6

---
+++ src/config/database.config.ts
@@ -0,0 +1,18 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+/**
+ * 数据库配置
+ */
+export const database = (): TypeOrmModuleOptions => ({
+    charset: 'utf8mb4',
+    logging: ['error'],
+    type: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'root',
+    password: '123456',
+    database: '3r',
+    synchronize: true,
+    autoLoadEntities: true,
+    // entities: []
+});
