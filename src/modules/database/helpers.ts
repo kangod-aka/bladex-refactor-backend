3ec57a6e2f05ea124cb89b5e23f5c1e4ac4c8c18
KangShen <yk625x@163.com>
2023-06-11 14:05:30 +0800

    1、修改自定义分页函数

--- src/modules/database/helpers.ts
+++ src/modules/database/helpers.ts
@@ -4,22 +4,22 @@ import { PaginateOptions, PaginateReturn } from './types';
 
 /**
  * 分页函数
- * @param qb 接收复用的SelectQueryBuilder
+ * @param queryBuilder 接收复用的SelectQueryBuilder
  * @param options 分页选项
  */
 export const paginate = async <E extends ObjectLiteral>
-        (qb: SelectQueryBuilder<E>, options: PaginateOptions): Promise<PaginateReturn<E>> => {
+(queryBuilder: SelectQueryBuilder<E>, options: PaginateOptions): Promise<PaginateReturn<E>> => {
     // 计算take和skip的值，并查询分页数据
     const start = options.page > 0 ? options.page - 1 : 0;
-    qb.take(options.page).skip(start * options.page);
-    const items = await qb.getMany();
+    queryBuilder.take(options.size).skip(start * options.size);
+    const items = await queryBuilder.getMany();
     // 查询数据总条数
-    const totalItems = await qb.getCount();
+    const totalItems = await queryBuilder.getCount();
     // 计算总页数
-    const totalPages = Math.ceil(totalItems / options.page);
+    const totalPages = Math.ceil(totalItems / options.size);
     // 计算当前页项目数量
-    const remainder = totalItems % options.page !== 0 ? totalItems % options.page : options.page;
-    const itemCount = options.page < totalPages ? options.page : remainder;
+    const remainder = totalItems % options.size !== 0 ? totalItems % options.size : options.size;
+    const itemCount = options.page < totalPages ? options.size : remainder;
     return {
         items,
         meta: {
